237e986fa41d5b3cd393447d7b350e0726823a4d
ablwr <ashley.blewer@gmail.com>
2014-10-21 15:31:28 -0400

    cleaned up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,4 @@
 # Testing plan of attack
-# creation_spec.rb - That the files we expect to be created are created and have a size larger than 0.
-# input_spec.rb - That not adding an input parameter shows the help message and any other cases where it should bomb out.
 # - That the right number of thumbnails are created for different parameters.
 # - That the WebVTT file includes the text WEBVTT, a NOTE (see issue #1), a first cue that matches the duration given, and the expected cue payload.
 
@@ -9,15 +7,4 @@ Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each { |f| require_rel
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.formatter = :documentation
-end
-
-def capture_stdout(&block)
-  original_stdout = $stdout
-  $stdout = fake = StringIO.new
-  begin
-    yield
-  ensure
-    $stdout = original_stdout
-  end
-  fake.string
 end
